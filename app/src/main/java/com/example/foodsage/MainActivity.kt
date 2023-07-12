aad80875c6c338d2e932f7b87c6e0d69f2013cc9
Fergus24 <fsinclairhouse@gmail.com>
2023-07-12 10:12:31 +0100

    basic login

--- app/src/main/java/com/example/foodsage/MainActivity.kt
+++ app/src/main/java/com/example/foodsage/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.foodsage
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,18 +16,37 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.foodsage.ui.theme.FoodSageTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var login: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FoodSageTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.activity_main)
+
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        login = findViewById(R.id.login)
+
+        login.setOnClickListener {
+            if (validateInput()) {
+                Toast.makeText(this, "Logged in successfully", Toast.LENGTH_SHORT).show()
+                //TODO: Implement your login logic here
             }
+        }
+    }
+
+    private fun validateInput(): Boolean {
+        return when {
+            username.text.isBlank() -> {
+                username.error = "Username cannot be empty"
+                false
+            }
+            password.text.isBlank() -> {
+                password.error = "Password cannot be empty"
+                false
+            }
+            else -> true
         }
     }
 }
